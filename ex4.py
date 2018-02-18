268490300cb39a85cea41027da5fc66de748284f
AndriiTorchuk <andriytv86@gmail.com>
2018-02-18 21:51:52 +0100

    small changes

--- ex4.py
+++ ex4.py
@@ -4,15 +4,15 @@ students = []
 def read_file ():
     try:
         f = open("Students.txt", "r")
-        for student in read_students(f):
-            student.append(student)
+        for student in f.readlines():
+            students.append(student)
         f.close()
     except Exception:
         print("Can't read file")
 
-def read_students(f):
+"""def read_students(f):
     for line in f:
-        yield line
+        yield line"""
 
 read_file()
 print(students)
